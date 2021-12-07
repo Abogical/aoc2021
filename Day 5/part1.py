5449cbeaa47b79db4d4f2fb8d5c7213ac8b13dc3
Abdelrahman Abdelrahman <git@abogic.al>
2021-12-07 23:37:42 +0200

    Day 5

---
+++ Day 5/part1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import re
+from typing import Tuple
+
+line_processor = re.compile(r'(\d+),(\d+) -> (\d+),(\d+)')
+
+
+more_than_ones: set[Tuple[int, int]] = set()
+ones: set[Tuple[int, int]] = set()
+counter = 0
+
+
+def between(n1, n2):
+    n_min, n_max = (n1, n2) if n1 <= n2 else (n2, n1)
+    return range(n_min, n_max+1)
+
+
+def set_ocean(index):
+    global counter
+    if index not in more_than_ones:
+        if index in ones:
+            counter += 1
+            more_than_ones.add(index)
+        else:
+            ones.add(index)
+
+
+with open("input.txt") as file:
+    for x1,y1,x2,y2 in (map(int, line_processor.match(line).groups()) for line in file):
+        if x1 == x2:
+            for y in between(y1, y2):
+                set_ocean((x1, y))
+        elif y1 == y2:
+            for x in between(x1, x2):
+                set_ocean((x, y1))
+
+print(counter)
